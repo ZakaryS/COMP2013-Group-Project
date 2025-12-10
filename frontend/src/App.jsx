66118de8b3410c82977bcd072e55e5d14199072a
ZakaryS <91573057+ZakaryS@users.noreply.github.com>
2025-12-10 18:25:01 -0500

    Did some shit
    LoginPage is done for now
    NotAuthorizedPage is done
    NotFoundPage is done
    Login route is done
    basic routing for App.jsx is pretty much done.
    Added missing dependencies
    Made some bread (it was good)
    centered a div

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import products from "./data/products";
-import GroceriesAppContainer from "./Components/GroceriesAppContainer";
+//import products from "./data/products";
+//import GroceriesAppContainer from "./Components/GroceriesAppContainer";
+import LoginPage from "./Components/LoginPage";
+import NotAuthorizedPage from "./Components/NotAuthorizedPage";
+import NotFoundPage from "./Components/NotFoundPage";
 
 function App() {
   return (
     <>
-      <GroceriesAppContainer products={products} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginPage/>}/>
+          <Route path="/not-authorized" element={<NotAuthorizedPage/>}/>
+          <Route path="*" element={<NotFoundPage/>}/>
+        </Routes>
+      </Router>
     </>
   );
 }
 
 export default App;
+//<Route path="/create-user" element={<ShreyasPage/>}/>
+//<Route path="/main" element={<LiaoPage/>}/>
+//<Route path="/add-product" element={<OmedyaPage/>}/>
+//<Route path="/edit-product" element={<RyanPage/>}/>
